727026ca6a1c3e19b37dac0e2f713e46336839c7
Kevin Mugele <kevin.mugele@gmail.com>
2021-09-22 10:12:17 -0400

    Create method for finding average age of players. Write test in model spec

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,4 +1,8 @@
 class Competition < ApplicationRecord
   has_many :participants
   has_many :teams, through: :participants
+
+  def average_player_age
+    teams.joins(:players).average(:age)
+  end
 end
--- spec/models/competition_spec.rb
+++ spec/models/competition_spec.rb
@@ -5,4 +5,30 @@ RSpec.describe Competition do
     it { should have_many(:participants) }
     it { should have_many(:teams).through(:participants) }
   end
-end 
+
+  before :each do
+    @comp1 = Competition.create!(name: "World Cup", location: "Qatar", sport: "Soccer")
+    @comp2 = Competition.create!(name: "Olympics", location: "Sochi", sport: "Many")
+    @team1 = Team.create!(hometown: "England", nickname: "The Three Lions")
+    @team2 = Team.create!(hometown: "USA", nickname: "USMNT")
+    @team3 = Team.create!(hometown: "New Jersey Devils", nickname: "Devils")
+    @player1 = @team1.players.create!(name: "Marcus Rashford", age: 21)
+    @player2 = @team1.players.create!(name: "Jadon Sancho", age: 20)
+    @player3 = @team2.players.create!(name: "Christian Pulisic", age: 21)
+    @player4 = @team2.players.create!(name: "Sergino Dest", age: 19)
+    @player5 = @team3.players.create!(name: "Jack Hughes", age: 19)
+    @player6 = @team3.players.create!(name: "Nico Hischier", age: 22)
+    @par1 = Participant.create!(team_id: @team1.id, competition_id: @comp1.id)
+    @par2 = Participant.create!(team_id: @team1.id, competition_id: @comp2.id)
+    @par3 = Participant.create!(team_id: @team2.id, competition_id: @comp1.id)
+    @par4 = Participant.create!(team_id: @team2.id, competition_id: @comp2.id)
+    @par5 = Participant.create!(team_id: @team3.id, competition_id: @comp1.id)
+    @par6 = Participant.create!(team_id: @team3.id, competition_id: @comp2.id)
+  end
+
+  describe 'average age for players' do
+    it '#average_player_age' do
+      expect(@comp1.average_player_age.round(2)).to eq(20.33)
+    end
+  end
+end
