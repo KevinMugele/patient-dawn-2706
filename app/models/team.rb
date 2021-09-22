baf2dc8ccd0823746cc90e60b2a487e24d0ea952
Kevin Mugele <kevin.mugele@gmail.com>
2021-09-22 09:58:48 -0400

    Create new participants and competition models. Added relationships to all

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,5 @@
 class Team < ApplicationRecord
   has_many :players
+  has_many :participants
+  has_many :competitions, through: :participants
 end
--- spec/models/participants_spec.rb
+++ spec/models/participants_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Participants do
+RSpec.describe Participant do
   describe 'relationships' do
     it { should belong_to(:competition) }
     it { should belong_to(:team) }
